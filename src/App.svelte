d4dc48a928a9fc84fd7ad6a9d76f0c75a322ed0d
Mike Boss <mikeboss@windowslive.com>
2019-10-30 01:28:20 +0100

    Initial graph working with VivaGraphJS though svg not correctly sized

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,26 @@
-<script>
-  import Search from './Search.svelte';
-</script>
-
 <div class="h-screen w-screen content-center">
-  <div class="flex justify-center w-full mt-16">
+  <div class="flex justify-center w-full pt-16">
     <h1 class="text-3xl font-light">Paper Graph</h1>
   </div>
-  <div class="flex justify-center w-full mt-8 md:mt-12 lg:mt-16">
-    <Search/>
+  <div class="flex justify-center h-full w-full mt-8 md:mt-12 lg:mt-16">
+    {#if clickedPaper}
+      <Search on:clickedPaper={createGraph}/>
+    {:else}
+      <Graph originalPaperId={originalPaperId}/>
+    {/if}
   </div>
 </div>
+
+<script>
+  import Search from './Search.svelte';
+  import Graph from './Graph.svelte';
+  
+  let clickedPaper = false;
+  let originalPaperId;
+
+  function createGraph(event) {
+    console.log(event.detail.text);
+    clickedPaper = true;
+    originalPaperId = event.detail.text;
+  }
+</script>
--- src/Search.svelte
+++ src/Search.svelte
@@ -1,22 +1,28 @@
 <div class="max-w-xs md:max-w-md lg:max-w-xl flex-grow flex-shrink-0">
-    <div class="relative h-10">
-        <input placeholder="Search for a paper" type="search" on:keyup={handleInput} id="site-search" aria-label="Search for papers" class="focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-200 py-2 pr-4 pl-10 block w-full appearance-none leading-normal ds-input">
+    <div class="relative h-10 my-4">
+        <input placeholder="Search for a paper" type="search" on:keyup={handleInput} id="site-search" aria-label="Search for papers" class="focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-300 py-2 pr-4 pl-10 block w-full appearance-none leading-normal ds-input">
         <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
             <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
             </svg>
         </div>
     </div>
-    {#each titles as title (title.id)}
-        <div class="my-4">
-            <p>{title.content}</p>
+    {#if titles}
+        <div class="flex flex-col">
+            {#each titles as title (title.id)}
+                <div on:click="{e => clickedPaper(title.id)}" class="bg-white rounded-lg shadow-xl p-4 my-2 hover:shadow-2xl hover:bg-gray-100">
+                    {title.content}
+                </div>
+            {/each}
         </div>
-    {/each}
+    {/if}
 </div>
 
 <script>
     import 'whatwg-fetch';
-    
+    import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
     var titles = [];
 
     var inputValue = "";
@@ -34,14 +40,16 @@
     }
 
     function evaluate(query) {
-        console.log(query)
-        var evaluatePath = "/evaluate?expr=" + query + "&attributes=Id,E";
+        let attributes = ['Id', 'E'];
+        let evaluatePath = "/evaluate?expr=" + query + "&attributes=" + attributes.join();
 
+        console.log(query)
         fetchEvaluate(evaluatePath).then(evaluation => {
             evaluation.entities.forEach(function(element) {
                 titles = [...titles, {id: element.Id, content: JSON.parse(element.E).DN}];
             });
-        })
+            console.log(titles);
+        });
     }
 
     function fetchEvaluate(evaluatePath) {
@@ -101,6 +109,12 @@
         })
         .catch(err => {
             console.error(err);
+        });
+    }
+
+    function clickedPaper(paperId) {
+        dispatch('clickedPaper', {
+            text: paperId
         });
     }
 </script>
