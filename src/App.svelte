effb6f4340f58aa1fdbc3cb0459d25974c192e55
Mike Boss <mikeboss@windowslive.com>
2019-11-03 01:56:43 +0100

    Added titlebar to setSubscriptionKey

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,26 @@
 <div class="w-screen h-screen">
-  {#if !subscriptionKey}
-    <div class="flex justify-center w-full pt-16">
-      <div class="max-w-xs md:max-w-md lg:max-w-xl flex-grow flex-shrink-0">
-        <input placeholder="Enter subscription key" type="input" on:keyup={setSubscriptionKey} id="site-search" aria-label="Search for papers" class="focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-300 py-2 px-4 block w-full appearance-none leading-normal ds-input">
-        <p>You can generate a new key <a class="text-blue-600" href="https://msr-apis.portal.azure-api.net/products/project-academic-knowledge">here.</a></p>
+    {#if !clickedPaper}
+      <div class="flex justify-center w-full pt-16">
+        <h1 class="text-3xl font-light">Paper Graph</h1>
       </div>
-    </div>
-  {:else}
-    <div>
-      {#if !clickedPaper}
-        <div class="flex justify-center w-full pt-16">
-          <h1 class="text-3xl font-light">Paper Graph</h1>
+    {/if}
+    {#if !subscriptionKey}
+      <div class="flex justify-center w-full mt-8">
+        <div class="max-w-xs md:max-w-md lg:max-w-xl flex-grow flex-shrink-0">
+          <input placeholder="Enter subscription key" type="input" on:keyup={setSubscriptionKey} id="site-search" aria-label="Search for papers" class="focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-300 py-2 px-4 block w-full appearance-none leading-normal ds-input">
+          <p>You can generate a new key <a class="text-blue-600" href="https://msr-apis.portal.azure-api.net/products/project-academic-knowledge">here.</a></p>
         </div>
-      {/if}
+      </div>
+    {:else} 
       <div class="w-full h-full">
-        <div class="flex justify-center {!clickedPaper ? 'mt-0' : 'mt-8'}">
+        <div class="flex justify-center mt-8">
           <Search {subscriptionKey} on:clickedPaper={createGraph}/>
         </div>
         <div class="top-0 w-full h-full z-0">
           <Graph {paperId} {subscriptionKey} maxNumberOfRequests={!clickedPaper ? 20 : 100}/>
         </div>
       </div>
-    </div>
-  {/if}
+    {/if}
 </div>
 
 <script>
